19e1fab05f73a7dab4953c394765bf10222d26c9
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-01-03 10:43:42 +0800

    fix syntaxe and log

--- generatePassword.js
+++ generatePassword.js
@@ -1,13 +1,11 @@
-import { prompt } from './prompt.js';
+import { prompt } from "./prompt.js";
 
 export function askPasswordLength() {
-  let length = Number(prompt('🔢 Combien de caractères ? (8-36)\n'));
-
-  console.log({ length });
+  let length = Number(prompt("🔢 Combien de caractères ? (8-36)\n"));
 
   if (length < 8 || length > 36 || Number.isNaN(length)) {
     throw new Error(
-      'La longueur du mot de passe doit être comprise entre 8 et 36 caractères.'
+      "La longueur du mot de passe doit être comprise entre 8 et 36 caractères."
     );
   }
 
@@ -15,39 +13,39 @@ export function askPasswordLength() {
 }
 
 export function askSpecialChars() {
-  let special = prompt('🔣 Caractères spéciaux ? (y/n)\n');
+  let special = prompt("🔣 Caractères spéciaux ? (y/n)\n");
 
-  if (special !== 'y' && special !== 'n') {
+  if (special !== "y" && special !== "n") {
     throw new Error('Veuillez répondre par "y" pour oui ou "n" pour non.');
   }
 
-  return special === 'y';
+  return special === "y";
 }
 
 export function askNumbers() {
-  let numbers = prompt('🔢 Chiffres ? (y/n)\n');
+  let numbers = prompt("🔢 Chiffres ? (y/n)\n");
 
-  if (numbers !== 'y' && numbers !== 'n') {
+  if (numbers !== "y" && numbers !== "n") {
     throw new Error('Veuillez répondre par "y" pour oui ou "n" pour non.');
   }
 
-  return numbers === 'y';
+  return numbers === "y";
 }
 
 export function askUppercase() {
-  let numbers = prompt('⬆️ Majuscules ? (y/n)\n');
+  let numbers = prompt("⬆️ Majuscules ? (y/n)\n");
 
-  if (numbers !== 'y' && numbers !== 'n') {
+  if (numbers !== "y" && numbers !== "n") {
     throw new Error('Veuillez répondre par "y" pour oui ou "n" pour non.');
   }
 
-  return numbers === 'y';
+  return numbers === "y";
 }
 
-export const LOWERCASE = 'abcdefghijklmnopqrstuvwxyz';
-export const UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-export const SPECIALS = '!@#$%^&*()';
-export const NUMBERS = '0123456789';
+export const LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+export const UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export const SPECIALS = "!@#$%^&*()";
+export const NUMBERS = "0123456789";
 
 export function generatePassword(length, special, numbers, uppercase) {
   let charset = LOWERCASE;
@@ -55,7 +53,7 @@ export function generatePassword(length, special, numbers, uppercase) {
   if (numbers) charset += NUMBERS;
   if (uppercase) charset += UPPERCASE;
 
-  let password = '';
+  let password = "";
   // check if password contain 1 majuscule
   for (let i = 0; i < length; i++) {
     let randomIndex = Math.floor(Math.random() * charset.length);
