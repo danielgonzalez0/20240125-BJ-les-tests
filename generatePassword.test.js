77e01649f5138734345538e1c16beb219b8114f1
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2024-01-03 10:20:17 +0800

    generate password

--- generatePassword.test.js
+++ generatePassword.test.js
@@ -1,36 +1,36 @@
-import { SPECIALS, generatePassword } from './generatePassword.js';
+import { SPECIALS, generatePassword } from "./generatePassword.js";
 
-// 🦁 Créer une function "test" qui prends deux arguments:
+// 🦁 Créer une fonction "test" qui prend deux arguments :
 // - "name" qui est une string
-// - "fn" qui est une function
-// Ensuite tu vas execute la function "fn"
-// - Si elle ne throw pas d'erreur, tu affiches "✅ Test ${name} passed"
-// - Si elle throw une erreur, tu affiches "❌ Test ${name} failed: ${error.message}"
+// - "fn" qui est une fonction
+// Ensuite, tu vas exécuter la fonction "fn"
+// - Si elle ne lance pas d'erreur, tu affiches "✅ Test ${name} passed"
+// - Si elle lance une erreur, tu affiches "❌ Test ${name} failed: ${error.message}"
 
 // 🦁 Wrap le code ci-dessous dans un appel de "test"
-// Test that 8 characters password is generated
+// Teste si un mot de passe de 8 caractères est généré
 const password = generatePassword(8, false, false, false);
 
 if (password.length !== 8) {
-  throw new Error('Password should have 8 characters');
+  throw new Error("Password should have 8 characters");
 }
 
-console.log('✅ Test 1 passed');
+console.log("✅ Test 1 passed");
 
 // 🦁 Wrap le code ci-dessous dans un appel de "test"
-// Test that password contain at least 1 special character if asked
+// Teste si le mot de passe contient au moins 1 caractère spécial si demandé
 const passwordWithSpecial = generatePassword(8, true, false, false);
 
-if (!passwordWithSpecial.split('').some((char) => SPECIALS.includes(char))) {
-  throw new Error('Password should contain specials characters');
+if (!passwordWithSpecial.split("").some((char) => SPECIALS.includes(char))) {
+  throw new Error("Password should contain special characters");
 }
 
 // 🦁 Wrap le code ci-dessous dans un appel de "test"
-// Test that password does NOT contain special character if not asked
+// Teste si le mot de passe ne contient PAS de caractère spécial si non demandé
 const passwordWithoutSpecial = generatePassword(8, false, false, false);
 
-if (passwordWithoutSpecial.split('').some((char) => SPECIALS.includes(char))) {
-  throw new Error('Password should NOT contain specials characters');
+if (passwordWithoutSpecial.split("").some((char) => SPECIALS.includes(char))) {
+  throw new Error("Password should NOT contain special characters");
 }
 
-console.log('✅ Test 2 passed');
+console.log("✅ Test 2 passed");
